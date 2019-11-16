f0c3fbd77c6985fa9b71b05d81d7d27ec385de4c
wkylin.w <wkylin.w@gmail.com>
2019-11-16 08:41:25 +0800

    array...

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let array = [
+  { rand: 1, id: 2 },
+  { rand: 1, id: 1 },
+  { rand: 1, id: 3 },
+  { rand: 1, id: 3 },
+];
 
-let array = ['apple','green'];
+let aRaa = array.sort((a, b) => a.id - b.id > 0 ? 1 : -1);
 
 console.log('array', array);
+console.log('aRaa', aRaa);
