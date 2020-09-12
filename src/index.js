54098d2745ff0b3cba6fc63ef75cebfdf60db633
wkylin <wkylin.w@gmail.com>
2020-09-12 10:02:49 +0800

    browser sync

---
+++ src/index.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function(){
+  console.log("The page is visible: ", document.visibilityState);
+});
+
+// function eventCounter() {
+//     var count = 0;
+//     return function() {
+//         count++;
+//         console.log(count);
+//     }
+// }
+// var counter = eventCounter();
+
+// console.log(counter());
--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,19 @@
+// window.addEventListener("load", function(){
+//   alert('aaa');
+//   console.log("The page is visible: ", document.visibilityState);
+// });
+
+
+// function eventCounter() {
+//     var count = 0;
+//     return function() {
+//         count++;
+//         console.log(count);
+//     }
+// }
+// var counter = eventCounter();
+
+// console.log(counter);
 
 // this.setState((prevState, props) => {
 // return {ischecked: !prevState.ischecked}
